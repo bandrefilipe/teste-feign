fc60f1c0a99cf87643bde0108eeea674114dc41c
André Filipe Barranco <b.andrefilipe@gmail.com>
2023-01-22 10:29:25 -0300

    example

--- feign/src/main/kotlin/com/example/feign/Executor.kt
+++ feign/src/main/kotlin/com/example/feign/Executor.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component
 @Component
 class Executor(client: Client) {
     init {
-        println("com.example.feign.Executor initiated")
+        println("com.example.feign.Executor initiated, client=$client")
         client.healthCheck().also { println(it) }
     }
 }
